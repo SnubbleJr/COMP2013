8fbee7e274f5e6a7f533dd9d569c033f79cf1865
Edward James <edwardjames@linuxmint.linuxmint-domain>
2013-11-01 15:05:03 +0000

    search.php

--- search.php
+++ search.php
@@ -13,6 +13,8 @@
     table { margin-top: 0.75em; }
     th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
     td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
+    tr:nth-child(even) {background: #CCC}
+    tr:nth-child(odd) {background: #FFF}
 </style>
 </head>
 <body>
